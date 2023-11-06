e0c8398ccfb85358f12cf2c2522f6bbd560587d9
Leejhy <jeehyeong0128@gmail.com>
2023-11-06 19:20:53 +0900

    123

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -11,8 +11,29 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*get_next_line(int fd)
+#define BUFFER_SIZE//지울예정
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> // O_RDONLY
+char    *get_next_line(int fd)
 {
-	read(fd, ?,);
+    int value_read;
+    if (fd < 0)//fd가 음수면 파일 열기 실패
+        return (NULL);
+    static char *str;
+    str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    value_read = read(fd, str, BUFFER_SIZE);
+	//value_read로 read로 읽는 버퍼 크기 받고 str에 fd에서 읽은 BUFFER_SIZE 받기
+    printf("%d\n",value_read);
+    return (str);
+}
+int main(void)
+{
+    int fd;
+    char *str;
+    fd = open("text.txt", O_RDONLY);// text읽기
+    str = get_next_line(fd);
+    printf("%s\n",str);
 }
