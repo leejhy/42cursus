50bfd09ec943fef6e0bfac70d9ad280d8e4e95a6
Junhyeong Lee <junhylee@c4r7s7.42seoul.kr>
2023-11-09 22:06:26 +0900

    123

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,26 +6,31 @@
 /*   By: junhylee <junhylee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 13:55:03 by junhylee          #+#    #+#             */
-/*   Updated: 2023/11/07 19:48:09 by junhylee         ###   ########.fr       */
+/*   Updated: 2023/11/09 22:00:05 by junhylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+
 # include <stdlib.h>
 # include <unistd.h>
 
-#define BUFFER_SIZE 2//지울예정
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h> // O_RDONLY
+#include <fcntl.h> // 
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1, size_t read_size);
-size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
+int		is_lf(char *buf);
+char	*get_str(char *str, char *buf, int read_size);
 
 #endif
