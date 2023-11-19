dd1d573ea3e6296634663834f79b192c42919fb3
Junhyeong Lee <junhylee@cx1r4s4.42seoul.kr>
2023-11-19 17:58:15 +0900

    done

--- newgnl/get_next_line.h
+++ newgnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: junhylee <junhylee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 15:32:41 by junhylee          #+#    #+#             */
-/*   Updated: 2023/11/19 17:47:30 by junhylee         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:55:31 by junhylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,15 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+void	ft_makenode(t_list **head, int fd);
+void	ft_delnode(t_list **head, int fd);
 char	*get_next_line(int fd);
+char	*ft_get_oneline(char *read_line);
+char	*ft_readline(int fd, char *buf, char **backup);
+int		is_nl(char *str);
+char	*ft_strchr(char *str);
 char	*ft_strdup(const char *s1);
-char	*ft_strchr(char *buf);
 char	*ft_freejoin(char *s1, char *s2);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*get_str(char *str, char *headbuf, char *buf, int read_size);
-void	ft_fd_lst(t_list **head, int fd);
-char	*ft_readline(int fd, char *buf, char **backup)
-char	*ft_getoneline(char *str, int *idx);
-int		is_lf(char *str);
 
 #endif
