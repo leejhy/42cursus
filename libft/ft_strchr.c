7d8546ea4775d7f2d41090e0a7420abc828e480d
Junhyeong Lee <junhylee@c4r5s8.42seoul.kr>
2023-11-28 15:05:00 +0900

    123

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: junhylee <junhylee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 15:48:09 by junhylee          #+#    #+#             */
-/*   Updated: 2023/11/19 15:49:48 by junhylee         ###   ########.fr       */
+/*   Updated: 2023/11/27 12:55:36 by junhylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, char c)
+char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
