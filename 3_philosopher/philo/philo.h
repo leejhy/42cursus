137ccbadb7d7a317f2e83848d08955520d795fe9
Leejhy <jeehyeong0128@gmail.com>
2024-01-18 09:03:28 +0900

    123

---
+++ 3_philosopher/philo/philo.h
@@ -0,0 +1,12 @@
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <memory.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
