04eb61250271eb6eab1000cd8863c7fa99da4e69
Junhyeong Lee <junhylee@cx1r4s8.42seoul.kr>
2024-04-08 21:38:29 +0900

    fast

---
+++ cpp_module/cpp02/ex00/Fixed.hpp
@@ -0,0 +1,15 @@
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+	private :
+	public :
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &fixed);
+		Fixed &operator= (const Fixed &fixed);
+}
+
+#endif
