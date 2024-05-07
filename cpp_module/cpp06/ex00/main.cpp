734b38cdbb32f7807275870f001c0483c37993e4
Leejhy <jeehyeong0128@gmail.com>
2024-05-07 09:12:58 +0900

    2

---
+++ cpp_module/cpp06/ex00/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char	a;
+	int		b;
+	float	c;
+	double	d;
+	
+	(void)argc;
+	(void)argv;
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+}
